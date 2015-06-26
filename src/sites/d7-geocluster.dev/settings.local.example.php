ab81b68490d0f015deec58db1146ac11708fe11e
eric paul <epaul@phase2technology.com>
2015-06-26 12:16:13 -0500

    Add starter settings file architecture.

---
+++ src/sites/d7-geocluster.dev/settings.local.example.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Include an environment-specific settings file for db config, etc.
+ */
+
+$databases['default']['default'] = array(
+  'driver' => 'mysql',
+  'database' => 'd7geocluster',
+  'username' => 'epaul',
+  'password' => 'd7geocluster',
+  'host' => 'localhost',
+);
